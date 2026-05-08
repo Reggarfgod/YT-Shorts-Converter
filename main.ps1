fb3ed8cd8768ea3b85c6436531dda3c1c273c11b
Anoop Singh <142715184+Reggarfgod@users.noreply.github.com>
2026-05-09 00:57:14 +0530

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -16,6 +16,7 @@ iex (iwr "$repo/core/ffmpeg.ps1" -UseBasicParsing).Content
 iex (iwr "$repo/core/videos.ps1" -UseBasicParsing).Content
 iex (iwr "$repo/core/modes.ps1" -UseBasicParsing).Content
 iex (iwr "$repo/core/converter.ps1" -UseBasicParsing).Content
+iex (iwr "$repo/core/ai_title.ps1" -UseBasicParsing).Content
 
 # =========================================================
 # MAIN LOOP
@@ -25,17 +26,49 @@ do {
 
     Show-Banner
 
+    # =====================================================
+    # SELECT VIDEO
+    # =====================================================
+
     $video = Get-VideoSelection
 
     if ($null -eq $video) {
         break
     }
 
+    # =====================================================
+    # GENERATE AI TITLE
+    # =====================================================
+
+    $titleText = Get-AutoTitle `
+        -VideoFile $video.FullName
+
+    Write-Host ""
+    Write-Host "================================================="
+    Write-Host "AI TITLE:"
+    Write-Host $titleText
+    Write-Host "================================================="
+    Write-Host ""
+
+    # =====================================================
+    # SELECT MODE
+    # =====================================================
+
     $modeData = Get-ModeSelection
 
     if ($null -eq $modeData) {
         break
     }
+
+    # =====================================================
+    # ADD TITLE TO VIDEO
+    # =====================================================
+
+    $modeData.Filter += ",drawtext=text='$titleText':fontcolor=white:fontsize=60:borderw=4:bordercolor=black:x=(w-text_w)/2:y=780"
+
+    # =====================================================
+    # START CONVERSION
+    # =====================================================
 
     Start-Conversion `
         -Video $video `
