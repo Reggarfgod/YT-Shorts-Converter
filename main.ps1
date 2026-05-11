a9d0879088af57fca934b4588049973be6d62c48
Anoop Singh <142715184+Reggarfgod@users.noreply.github.com>
2026-05-12 01:54:05 +0530

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -17,6 +17,7 @@ iex (iwr "$repo/core/videos.ps1" -UseBasicParsing).Content
 iex (iwr "$repo/core/modes.ps1" -UseBasicParsing).Content
 iex (iwr "$repo/core/converter.ps1" -UseBasicParsing).Content
 iex (iwr "$repo/core/trim.ps1" -UseBasicParsing).Content
+iex (iwr "$repo/core/overlay.ps1" -UseBasicParsing).Content
 
 # =========================================================
 # MAIN LOOP
@@ -60,12 +61,47 @@ do {
     # START CONVERSION
     # =====================================================
 
-    Start-Conversion `
+    $convertedFile = Start-Conversion `
         -Video $video `
         -RatioName $modeData.Ratio `
         -Filter $modeData.Filter `
         -StartTime $trimData.Start `
         -EndTime $trimData.End
+
+    # =====================================================
+    # ADD OVERLAY
+    # =====================================================
+
+    Write-Host ""
+    Write-Host "================================================="
+    Write-Host "ADD LIKE / SUBSCRIBE OVERLAY?"
+    Write-Host "================================================="
+    Write-Host "1. Yes"
+    Write-Host "2. No"
+    Write-Host ""
+
+    $overlayChoice = Read-Host "Enter choice"
+
+    if ($overlayChoice -eq "1") {
+
+        $finalOutput = $convertedFile.Replace(
+            ".mp4",
+            "_Overlay.mp4"
+        )
+
+        Add-LikeSubscribeOverlay `
+        -InputVideo $convertedFile `
+        -OutputVideo $finalOutput
+
+        Write-Host ""
+        Write-Host "FINAL VIDEO:"
+        Write-Host $finalOutput
+        Write-Host ""
+    }
+
+    # =====================================================
+    # FINISHED
+    # =====================================================
 
     Write-Host ""
     Write-Host "================================================="
