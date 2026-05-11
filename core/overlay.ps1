5f5420abddff0b8c2c1969d15ed30817f802f5e0
Anoop Singh <142715184+Reggarfgod@users.noreply.github.com>
2026-05-12 02:02:44 +0530

    Update overlay.ps1

--- core/overlay.ps1
+++ core/overlay.ps1
@@ -12,6 +12,13 @@ function Start-Conversion {
 
     $outputFolder = "Converted_Output"
 
+    if (!(Test-Path $outputFolder)) {
+
+        New-Item `
+        -ItemType Directory `
+        -Path $outputFolder | Out-Null
+    }
+
     $inputFile = $Video.FullName
 
     $outputFile = Join-Path `
@@ -31,24 +38,22 @@ function Start-Conversion {
         Write-Host ""
 
         # =================================================
-        # ADD OVERLAY TO FILTER
+        # FILTER FIX
         # =================================================
 
         if ($AddOverlay -eq "1") {
 
-            $overlayFilter = @"
+            $finalFilter = @"
 $Filter,
 drawbox=x=20:y=1650:w=1040:h=180:color=black@0.45:t=fill,
-drawtext=fontfile=C\:/Windows/Fonts/arialbd.ttf:text='👍 LIKE':fontcolor=white:fontsize=55:x=80:y=1700,
-drawtext=fontfile=C\:/Windows/Fonts/arialbd.ttf:text='💬 COMMENT':fontcolor=white:fontsize=55:x=330:y=1700,
-drawtext=fontfile=C\:/Windows/Fonts/arialbd.ttf:text='🔔 SUBSCRIBE':fontcolor=red:fontsize=60:x=620:y=1695[outv]
+drawtext=fontfile='C\:/Windows/Fonts/arialbd.ttf':text='LIKE':fontcolor=white:fontsize=55:x=80:y=1700,
+drawtext=fontfile='C\:/Windows/Fonts/arialbd.ttf':text='COMMENT':fontcolor=white:fontsize=55:x=330:y=1700,
+drawtext=fontfile='C\:/Windows/Fonts/arialbd.ttf':text='SUBSCRIBE':fontcolor=red:fontsize=60:x=620:y=1695
 "@
-
-            $finalFilter = $overlayFilter
         }
         else {
 
-            $finalFilter = "$Filter[outv]"
+            $finalFilter = $Filter
         }
 
         # =================================================
@@ -59,15 +64,14 @@ drawtext=fontfile=C\:/Windows/Fonts/arialbd.ttf:text='🔔 SUBSCRIBE':fontcolor=
         -ss $StartTime `
         -to $EndTime `
         -i "$inputFile" `
-        -filter_complex $finalFilter `
-        -map "[outv]" `
-        -map 0:a? `
+        -vf $finalFilter `
         -r 60 `
         -s 1080x1920 `
         -c:v libx264 `
         -preset medium `
         -crf 20 `
         -pix_fmt yuv420p `
+        -map 0:a? `
         -c:a aac `
         -b:a 192k `
         -shortest `
