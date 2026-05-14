9cd378ea5be2b7a1b4d4b2db1cd5b6659d70791d
Anoop Singh <142715184+Reggarfgod@users.noreply.github.com>
2026-05-14 19:50:03 +0530

    Update converter.ps1

--- core/converter.ps1
+++ core/converter.ps1
@@ -11,6 +11,17 @@ function Start-Conversion {
 
     $outputFolder = "Converted_Output"
 
+    # =====================================================
+    # CREATE OUTPUT FOLDER
+    # =====================================================
+
+    if (!(Test-Path $outputFolder)) {
+
+        New-Item `
+        -ItemType Directory `
+        -Path $outputFolder | Out-Null
+    }
+
     $inputFile = $Video.FullName
 
     $outputFile = Join-Path `
@@ -29,27 +40,35 @@ function Start-Conversion {
         Write-Host "End Time   : $EndTime"
         Write-Host ""
 
-ffmpeg -y `
--ss $StartTime `
--to $EndTime `
--i "$inputFile" `
--filter_complex $Filter `
--map "[outv]" `
--map 0:a? `
--r 60 `
--s 1080x1920 `
--c:v libx264 `
--preset medium `
--crf 20 `
--pix_fmt yuv420p `
--c:a aac `
--b:a 192k `
--shortest `
-"$outputFile"
+        Write-Host "================================================="
+        Write-Host "GPU RENDERING ENABLED (NVENC)"
+        Write-Host "================================================="
+        Write-Host ""
+
+        ffmpeg -y `
+        -hwaccel cuda `
+        -ss $StartTime `
+        -to $EndTime `
+        -i "$inputFile" `
+        -filter_complex $Filter `
+        -map "[outv]" `
+        -map 0:a? `
+        -r 60 `
+        -s 1080x1920 `
+        -c:v h264_nvenc `
+        -preset p5 `
+        -cq 20 `
+        -b:v 0 `
+        -pix_fmt yuv420p `
+        -c:a aac `
+        -b:a 192k `
+        -movflags +faststart `
+        -shortest `
+        "$outputFile"
 
         Write-Host ""
         Write-Host "================================================="
-        Write-Host " CONVERSION COMPLETE!"
+        Write-Host "CONVERSION COMPLETE!"
         Write-Host "================================================="
         Write-Host ""
         Write-Host "Saved To:"
