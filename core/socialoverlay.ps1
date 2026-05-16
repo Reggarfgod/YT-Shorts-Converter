bf9d075f97abc249f46ffd7d0f19800b2f88b374
Anoop Singh <142715184+Reggarfgod@users.noreply.github.com>
2026-05-16 14:44:29 +0530

    Update socialoverlay.ps1

--- core/socialoverlay.ps1
+++ core/socialoverlay.ps1
@@ -32,121 +32,35 @@ function Get-SocialOverlayFilter {
 
     switch ($position) {
 
-        "1" {
+        "1" { $baseY = 120 }
+        "2" { $baseY = 700 }
+        "3" { $baseY = 1200 }
 
-            $baseY = 120
-        }
-
-        "2" {
-
-            $baseY = 700
-        }
-
-        "3" {
-
-            $baseY = 1200
-        }
-
-        default {
-
-            $baseY = 1200
-        }
+        default { $baseY = 1200 }
     }
 
     # =====================================================
-    # ANIMATION TIMINGS
-    # =====================================================
-
-    $likeTime = 0
-    $commentTime = 1.2
-    $shareTime = 2.4
-    $subscribeTime = 3.6
-
-    # =====================================================
-    # FILTER
+    # CLEAN SINGLE LINE FILTER
     # =====================================================
 
     $filter = @"
 
-drawbox=
-x=140:
-y=$baseY:
-w=550:
-h=90:
-color=white@0.92:
-t=fill:
-enable='between(t,$likeTime,$likeTime+1.2)',
+drawbox=x=140:y=$baseY:w=550:h=90:color=white@0.92:t=fill:enable='between(t,0,1.2)',
+drawtext=fontfile='C\:/Windows/Fonts/arialbd.ttf':text='LIKE':fontcolor=black:fontsize=42:x=220:y=$($baseY+22):enable='between(t,0,1.2)',
 
-drawbox=
-x=140:
-y=$($baseY+110):
-w=550:
-h=90:
-color=white@0.92:
-t=fill:
-enable='between(t,$commentTime,$commentTime+1.2)',
+drawbox=x=140:y=$($baseY+110):w=550:h=90:color=white@0.92:t=fill:enable='between(t,1.2,2.4)',
+drawtext=fontfile='C\:/Windows/Fonts/arialbd.ttf':text='COMMENT':fontcolor=black:fontsize=42:x=220:y=$($baseY+132):enable='between(t,1.2,2.4)',
 
-drawbox=
-x=140:
-y=$($baseY+220):
-w=550:
-h=90:
-color=white@0.92:
-t=fill:
-enable='between(t,$shareTime,$shareTime+1.2)',
+drawbox=x=140:y=$($baseY+220):w=550:h=90:color=white@0.92:t=fill:enable='between(t,2.4,3.6)',
+drawtext=fontfile='C\:/Windows/Fonts/arialbd.ttf':text='SHARE':fontcolor=black:fontsize=42:x=220:y=$($baseY+242):enable='between(t,2.4,3.6)',
 
-drawbox=
-x=140:
-y=$($baseY+330):
-w=620:
-h=90:
-color=red@0.98:
-t=fill:
-enable='between(t,$subscribeTime,$subscribeTime+2)',
-
-drawtext=
-fontfile='C\:/Windows/Fonts/arialbd.ttf':
-text='👍 Like':
-fontcolor=black:
-fontsize=42:
-x=220:
-y=$($baseY+22):
-enable='between(t,$likeTime,$likeTime+1.2)',
-
-drawtext=
-fontfile='C\:/Windows/Fonts/arialbd.ttf':
-text='💬 Comment':
-fontcolor=black:
-fontsize=42:
-x=220:
-y=$($baseY+132):
-enable='between(t,$commentTime,$commentTime+1.2)',
-
-drawtext=
-fontfile='C\:/Windows/Fonts/arialbd.ttf':
-text='↗ Share':
-fontcolor=black:
-fontsize=42:
-x=220:
-y=$($baseY+242):
-enable='between(t,$shareTime,$shareTime+1.2)',
-
-drawtext=
-fontfile='C\:/Windows/Fonts/arialbd.ttf':
-text='🔔 Subscribe':
-fontcolor=white:
-fontsize=44:
-x=220:
-y=$($baseY+352):
-enable='between(t,$subscribeTime,$subscribeTime+2)'
+drawbox=x=140:y=$($baseY+330):w=620:h=90:color=red@0.98:t=fill:enable='between(t,3.6,6)',
+drawtext=fontfile='C\:/Windows/Fonts/arialbd.ttf':text='SUBSCRIBE':fontcolor=white:fontsize=44:x=220:y=$($baseY+352):enable='between(t,3.6,6)'
 
 "@
 
-    $filter = `
-    $filter.Replace("`r","")
-
-    $filter = `
-    $filter.Replace("`n","")
+    $filter = $filter.Replace("`r","")
+    $filter = $filter.Replace("`n","")
 
     return $filter
 }
