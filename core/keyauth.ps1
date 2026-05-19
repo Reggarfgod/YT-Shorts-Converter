99c1a8ed243d6b84d8e632d2ddf96879cf493b6b
Anoop Singh <142715184+Reggarfgod@users.noreply.github.com>
2026-05-19 18:20:54 +0530

    Update keyauth.ps1

--- core/keyauth.ps1
+++ core/keyauth.ps1
@@ -1,1 +1,61 @@
+function verifyKey(key) {
 
+    // =================================================
+    // LOAD DATABASE
+    // =================================================
+
+    const data = loadKeys();
+
+    // =================================================
+    // INVALID KEY
+    // =================================================
+
+    if (!data.keys[key]) {
+
+        console.log(
+            `INVALID KEY: ${key}`
+        );
+
+        return false;
+    }
+
+    // =================================================
+    // MASTER KEY CHECK
+    // =================================================
+
+    if (data.keys[key].master === true) {
+
+        console.log(
+            `MASTER KEY USED: ${key}`
+        );
+
+        return true;
+    }
+
+    // =================================================
+    // ALREADY USED
+    // =================================================
+
+    if (data.keys[key].used) {
+
+        console.log(
+            `USED KEY: ${key}`
+        );
+
+        return false;
+    }
+
+    // =================================================
+    // MARK USED
+    // =================================================
+
+    data.keys[key].used = true;
+
+    saveKeys(data);
+
+    console.log(
+        `VALID KEY USED: ${key}`
+    );
+
+    return true;
+}
